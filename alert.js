0c2747817174ecd80f64f6ca92e3d5bd99556955
EdwinQAQ <145346972+EdwinQAQ@users.noreply.github.com>
2023-10-14 20:52:40 +1100

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,4 +1,4 @@
 var vg_1 = "alert.vega";
-vegaEmbed("#alert", vg_1).then(function(result) {actions: false
+vegaEmbed("#alert", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
