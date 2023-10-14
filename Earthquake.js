a158c4703c7853f606ea0ba6e076099814d4cd10
EdwinQAQ <145346972+EdwinQAQ@users.noreply.github.com>
2023-10-14 21:15:32 +1100

    Update Earthquake.js

--- Earthquake.js
+++ Earthquake.js
@@ -1,4 +1,4 @@
 var vg_1 = "Earthquake.vega";
-vegaEmbed("#proportional_symbol_map", vg_1).then(function(result) {
+vegaEmbed("#proportional_symbol_map", vg_1, actions={false}).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
